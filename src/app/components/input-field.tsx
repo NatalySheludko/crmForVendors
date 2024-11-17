a56e6df0723d6f4b070c9d179ab989f971291e09
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-11-17 16:35:32 +0200

    add "prop as" support into input

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -6,9 +6,18 @@ import React from 'react';
 export interface InputFieldProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  as?: 'input' | 'select' | 'textarea';
+  name: string;
+  children?: React.ReactNode;
 }
 
-export default function InputField({ label, id, ...rest }: InputFieldProps) {
+export default function InputField({
+  label,
+  id,
+  as = 'input',
+  children,
+  ...rest
+}: InputFieldProps) {
   return (
     <div className="flex flex-col">
       {label && (
@@ -17,10 +26,13 @@ export default function InputField({ label, id, ...rest }: InputFieldProps) {
         </label>
       )}
       <Field
+        as={as}
         {...rest}
         id={id}
         className="p-3 h-11 text-sm rounded border border-gray-300 shadow"
-      />
+      >
+        {children}
+      </Field>
     </div>
   );
 }
