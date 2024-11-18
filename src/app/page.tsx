d46bbeb9d6fd75931ab127e75300047989d1a0fa
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-11-18 15:25:55 +0200

    add redirect to /dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import ToCompanyButton from './components/to-company-button';
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <ToCompanyButton />
     </main>
   );
 }
